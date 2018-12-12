c0833c1dd8306de09ca542e6c44e623fbb5ffae0
Gabriel Possemozer <gossemozer2012@gmail.com>
2018-12-12 17:21:50 -0200

    Projeto Criado

---
+++ src/main/java/com/possemozergabriel/cursomt/CursomtApplication.java
@@ -0,0 +1,14 @@
+package com.possemozergabriel.cursomt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomtApplication.class, args);
+	}
+
+}
+
