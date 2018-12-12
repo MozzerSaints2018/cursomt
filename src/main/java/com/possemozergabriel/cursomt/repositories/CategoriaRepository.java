93872fe3de103a239c9a8a18e4f3d79725cc30f5
Gabriel Possemozer <gossemozer2012@gmail.com>
2018-12-12 21:02:24 -0200

    Criando repository e service para categoria

---
+++ src/main/java/com/possemozergabriel/cursomt/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.possemozergabriel.cursomt.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.possemozergabriel.cursomt.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
