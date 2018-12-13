7f741865a911d80a5b5b0f0a0f3e05bd33aa068c
Gabriel Possemozer <gossemozer2012@gmail.com>
2018-12-13 16:37:10 -0200

    Tratamento de Excecao

---
+++ src/main/java/com/possemozergabriel/cursomt/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.possemozergabriel.cursomt.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class) // Anotação indica que o tratador de exceção é do tipo que esta como parâmetro
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
